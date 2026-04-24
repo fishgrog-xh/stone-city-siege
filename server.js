c3493626b38a23c56a90c2b5fe0e2d6d20404b95
XH <2734664791@qq.com>
2026-04-24 20:47:37 +0800

    fix: 添加 start 脚本指向 server.js

--- server.js
+++ server.js
@@ -324,5 +324,5 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 3000;
-server.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
